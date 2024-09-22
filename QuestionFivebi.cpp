206418763b6191f43d497c11f28c27ebd1d724d2
RaymondABESIGA <146478989+RaymondAbesiga@users.noreply.github.com>
2024-09-22 10:43:55 +0300

    Add files via upload

---
+++ QuestionFivebi.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "ABESIGA RAYMOND" << endl;
+    return 0;
+}
---
+++ QuestionFivebii.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "Multiples of 3 from 1 to 20:" << endl;
+
+    for (int i = 1; i <= 20; ++i) {
+        if (i % 3 == 0) {
+            cout << i << endl; // Print the multiple of 3
+        }
+    }
+
+    return 0;
+}
